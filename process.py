be6dbc9b62ae3c2dd178988eb446fa98bef08068
SusanBuggell <92121306+SusanBuggell@users.noreply.github.com>
2021-11-15 21:43:34 -0500

    Commented back in 1st 1/2 of .py file

--- process.py
+++ process.py
@@ -1,22 +1,22 @@
-# # opens file
-# log_file = open("um-server-01.txt")
+# opens file
+log_file = open("um-server-01.txt")
 
-# # defines sales_reports taking in the opened file as a parameter
-# def sales_reports(log_file):
-#     # reads in the input file one line at a time
-#     for line in log_file:
-#         # removes trailing whitespace from the line and returns a copy
-#         line = line.rstrip()
-#         # defines day as the indices from 0-3 in the line
-#         day = line[0:3]
-#         # if the values if the indices in day equal "Mon"
-#         if day == "Mon":
-#             # print the line from the original file to sales_reports 
-#             print(line)
+# defines sales_reports taking in the opened file as a parameter
+def sales_reports(log_file):
+    # reads in the input file one line at a time
+    for line in log_file:
+        # removes trailing whitespace from the line and returns a copy
+        line = line.rstrip()
+        # defines day as the indices from 0-3 in the line
+        day = line[0:3]
+        # if the values if the indices in day equal "Mon"
+        if day == "Mon":
+            # print the line from the original file to sales_reports 
+            print(line)
 
-# # prints out the entries in the file that meet the criteria 
-# sales_reports(log_file)
-# log_file.close();
+# prints out the entries in the file that meet the criteria 
+sales_reports(log_file)
+log_file.close();
 
 log_file = open('um-server-01.txt')
 
